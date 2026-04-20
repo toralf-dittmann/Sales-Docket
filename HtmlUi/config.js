c720886fa91eec049381b94f821c33577c345d03
toralf.dittmann <t@framesoft.com>
2026-04-20 18:29:46 +1200

    Point Sales Docket HtmlUi to anonymous web app deployment

--- HtmlUi/config.js
+++ HtmlUi/config.js
@@ -1,3 +1,3 @@
 window.SALES_DOCKET_CONFIG = {
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbx_6lOzapJAiFl8z-FwZ0t9_-r_U6ybAZzFfPSisDOEh3xqfgMeAiBKL-PLXF7IRnw7ew/exec'
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbycs_HryjjjU2jnLaWcD7IiXT4qS08BP5R84NtDwoYHcpEBcpJqlONa__ihdHXnPD7MNQ/exec'
 };
